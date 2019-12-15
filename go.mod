50f622024bf43da96aec5a628dd706be7f3185f9
Charles Korn <me@charleskorn.com>
2019-12-15 19:12:49 +1100

    Add structured logging and gracefully handle shutdown.

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/onsi/ginkgo v1.10.3
 	github.com/onsi/gomega v1.7.1
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
 )
--- server/cmd/application.go
+++ server/cmd/application.go
@@ -1,23 +1,79 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/batect/abacus/server/api"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	initLogging()
+	srv := createServer(getPort())
+	runServer(srv)
+}
+
+func initLogging() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetReportCaller(true)
+}
+
+func getPort() string {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("PORT environment variable is not set.")
+		logrus.Fatal("PORT environment variable is not set.")
 	}
 
-	address := fmt.Sprintf(":%s", port)
+	return port
+}
 
-	http.HandleFunc("/ping", api.Ping)
-	log.Fatal(http.ListenAndServe(address, nil))
+func createServer(port string) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", api.Ping)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
+
+	return srv
+}
+
+func runServer(srv *http.Server) {
+	connectionDrainingFinished := shutdownOnInterrupt(srv)
+
+	logrus.WithField("address", srv.Addr).Info("Server starting.")
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		logrus.WithField("err", err).Fatal("Could not start HTTP server.")
+	}
+
+	<-connectionDrainingFinished
+
+	logrus.Info("Server shut down.")
+}
+
+func shutdownOnInterrupt(srv *http.Server) chan struct{} {
+	connectionDrainingFinished := make(chan struct{})
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		logrus.Info("Interrupt received, draining connections...")
+
+		if err := srv.Shutdown(context.Background()); err != nil {
+			logrus.WithField("err", err).Error("Shutting down HTTP server failed.")
+		}
+
+		close(connectionDrainingFinished)
+	}()
+
+	return connectionDrainingFinished
 }
