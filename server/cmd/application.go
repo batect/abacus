89f08b3e8f066bedd0c84cc061855066b80bb13b
Charles Korn <me@charleskorn.com>
2019-12-15 18:33:54 +1100

    Listen on the port given in PORT.
    See https://cloud.google.com/run/docs/reference/container-contract.

--- server/cmd/application.go
+++ server/cmd/application.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/batect/abacus/server/api"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT environment variable is not set.")
+	}
+
+	address := fmt.Sprintf(":%s", port)
+
 	http.HandleFunc("/ping", api.Ping)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	log.Fatal(http.ListenAndServe(address, nil))
 }
