686cf5a2897fd9dcfa4d0852857ebd995fb8b41d
Charles Korn <me@charleskorn.com>
2019-12-21 20:13:15 +1100

    Remove unnecessary creation of container image registry.
    We only needed this when we needed to set permissions on the bucket.

--- infra/bootstrap/image_registry.tf
+++ infra/bootstrap/image_registry.tf
@@ -1,18 +1,4 @@
-# See https://github.com/terraform-providers/terraform-provider-google/issues/4184 for an explanation of this.
-
 resource "google_project_service" "container_registry" {
   service = "containerregistry.googleapis.com"
   project = google_project.project.project_id
-}
-
-resource "null_resource" "initialise_container_registry" {
-  provisioner "local-exec" {
-    command = <<EOF
-      docker-credential-gcr configure-docker && \
-      (echo 'FROM scratch'; echo 'LABEL maintainer=charleskorn.com') | docker build -t gcr.io/${google_project_service.container_registry.project}/scratch:latest - && \
-      docker push gcr.io/${google_project_service.container_registry.project}/scratch:latest
-EOF
-  }
-
-  depends_on = [google_project_service.container_registry]
 }
--- infra/bootstrap/provider.tf
+++ infra/bootstrap/provider.tf
@@ -2,7 +2,3 @@ provider "google" {
   version     = "~> 3.2.0"
   credentials = "${path.module}/../../.creds/gcp_service_account_bootstrap_infra.json"
 }
-
-provider "null" {
-  version = "~> 2.1"
-}
