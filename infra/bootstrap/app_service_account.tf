8e31cf473f6830879d9657452290833b8d911064
Charles Korn <me@charleskorn.com>
2020-02-12 20:32:23 +1300

    Remove unnecessary application roles Terraform module.

--- infra/bootstrap/app_service_account.tf
+++ infra/bootstrap/app_service_account.tf
@@ -36,9 +36,3 @@ resource "google_service_account_iam_policy" "app" {
   service_account_id = google_service_account.app.name
   policy_data        = data.google_iam_policy.app_service_account.policy_data
 }
-
-module "application_roles" {
-  source = "../modules/application_roles"
-
-  project_id = google_project.project.project_id
-}
