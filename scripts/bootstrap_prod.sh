13570b4388c6fb7d578b86e6b13ff77282a5fb1c
Charles Korn <me@charleskorn.com>
2020-05-19 19:34:36 +1000

    Add helper script to run bootstrapping for prod environment.

---
+++ scripts/bootstrap_prod.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+rm -f infra/bootstrap/terraform.tfstate
+./batect --config-vars-file=batect.prod.yml setupBootstrapTerraform
+./batect --config-vars-file=batect.prod.yml importBootstrapState
+./batect --config-vars-file=batect.prod.yml planBootstrapTerraform
+
+read -p "Are you sure you want to apply the plan above? (y/N) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  ./batect --config-vars-file=batect.prod.yml applyBootstrapTerraform
+else
+  echo "Cancelled."
+  exit 1
+fi
